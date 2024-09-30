387ac7f464daad920ad1927116b21db3196c256d
simochee <lollipop@simochee.net>
2024-10-01 03:02:48 +0900

    feat: i18n

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "wxt";
 
 export default defineConfig({
 	manifest: {
-		name: "X2B",
-		description: "Convert X intent links to Bluesky intent links automatically",
+		default_locale: "en",
+		name: "__MSG_extName__",
+		description: "__MSG_extDescription__",
 		permissions: ["activeTab"],
 		host_permissions: ["*://twitter.com/intent/*", "*://x.com/intent/*"],
 	},
